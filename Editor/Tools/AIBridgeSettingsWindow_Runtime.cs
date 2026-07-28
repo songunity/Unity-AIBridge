97ce806cbc95787842f33bbed06c22f6fbe4cd05
songhe <songunreal@gmail.com>
2026-07-28 15:56:28 +0800

    fix(runtime): 局域网模式默认开启 UDP 发现

--- Editor/Tools/AIBridgeSettingsWindow_Runtime.cs
+++ Editor/Tools/AIBridgeSettingsWindow_Runtime.cs
@@ -138,10 +138,7 @@ namespace AIBridge.Editor
                 if (networkMode != currentNetworkMode)
                 {
                     settings.HttpBindAddress = networkMode == 0 ? "127.0.0.1" : "0.0.0.0";
-                    if (networkMode == 0)
-                    {
-                        settings.EnableLanDiscovery = false;
-                    }
+                    settings.EnableLanDiscovery = networkMode == 1;
                 }
 
                 settings.AuthToken = EditorGUILayout.DelayedTextField(
